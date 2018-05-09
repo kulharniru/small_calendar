77789ac2a6138a89aab06a08e0abcc3f779d561e
Unknown <zen.lednik7@gmail.com>
2018-05-09 11:20:03 +0200

    Created SmallCalendarStyle InheritedWidget

--- lib/src/small_calendar/all.dart
+++ lib/src/small_calendar/all.dart
@@ -2,3 +2,4 @@ export 'style/all.dart';
 export 'callbacks.dart';
 export 'small_calendar.dart';
 export 'small_calendar_data.dart';
+export 'small_calendar_style.dart';
--- lib/src/small_calendar/small_calendar.dart
+++ lib/src/small_calendar/small_calendar.dart
@@ -3,32 +3,20 @@ import 'package:meta/meta.dart';
 
 import 'package:small_calendar/src/data/all.dart';
 
-import 'style/all.dart';
 import 'calendar_day.dart';
 import 'callbacks.dart';
 import 'small_calendar_data_propagator.dart';
+import 'small_calendar_style.dart';
 import 'weekday_indicator.dart';
 
 class SmallCalendar extends StatelessWidget {
   SmallCalendar({
     @required this.month,
-    @required this.showWeekdayIndication,
-    @required this.weekdayIndicationStyle,
-    @required this.dayStyle,
     this.onDaySelected,
   });
 
   /// Month that is represented in this [SmallCalendar].
   final DateTime month;
-
-  /// If true weekday indication will be shown.
-  final bool showWeekdayIndication;
-
-  /// Style of weekday indication.
-  final WeekdayIndicationStyle weekdayIndicationStyle;
-
-  /// Style of a day inside this [SmallCalendar].
-  final DayStyle dayStyle;
 
   /// Called whenever user selects a day.
   final DateCallback onDaySelected;
@@ -50,7 +38,7 @@ class SmallCalendar extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Widget> columnItems = <Widget>[];
 
-    if (showWeekdayIndication) {
+    if (SmallCalendarStyle.of(context).showWeekdayIndication) {
       columnItems.add(_buildWeekdayIndication(context));
     }
 
@@ -63,8 +51,12 @@ class SmallCalendar extends StatelessWidget {
 
   Widget _buildWeekdayIndication(BuildContext context) {
     return new Container(
-      color: weekdayIndicationStyle.backgroundColor,
-      height: weekdayIndicationStyle.weekdayIndicationHeight,
+      color:
+          SmallCalendarStyle.of(context).weekdayIndicationStyle.backgroundColor,
+      height: SmallCalendarStyle
+          .of(context)
+          .weekdayIndicationStyle
+          .weekdayIndicationHeight,
       child: new Row(
         children: _generateWeekdayIndicationDays(context)
             .map<Widget>((weekday) => new Expanded(
@@ -72,7 +64,8 @@ class SmallCalendar extends StatelessWidget {
                   text: SmallCalendarDataPropagator
                       .of(context)
                       .dayNamesMap[weekday],
-                  weekdayIndicationStyle: weekdayIndicationStyle,
+                  weekdayIndicationStyle:
+                      SmallCalendarStyle.of(context).weekdayIndicationStyle,
                 )))
             .toList(),
       ),
@@ -104,7 +97,7 @@ class SmallCalendar extends StatelessWidget {
                   dayData:
                       SmallCalendarDataPropagator.of(context).getDayData(day),
                   isExtended: day.month != month.month,
-                  style: dayStyle,
+                  style: SmallCalendarStyle.of(context).dayStyle,
                   onPressed: onDaySelected,
                 ),
               ))
